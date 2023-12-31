df7361a787283c88d07e84c03e4dc1fe56c79c2c
Ludaro <waschmaschvanlu@gmail.com>
2023-12-31 17:30:53 +0100

    made esx version compliant, (works with 1.7.5 and above)

--- client.lua
+++ client.lua
@@ -1,6 +1,14 @@
-ESX = exports["es_extended"]:getSharedObject()
-
 local isCooldownActive = false
+if (GetResourceState("es_extended") == "started") then
+  if (exports["es_extended"] and exports["es_extended"].getSharedObject) then
+    ESX = exports["es_extended"]:getSharedObject()
+  else
+    TriggerEvent("esx:getSharedObject", function(obj) ESX = obj end)
+  end
+end
+
+
+
 
 Citizen.CreateThread(function()
     while true do
