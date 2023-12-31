26cc380747f6352af8fb904859cdf4abe7c75be1
Ludaro <waschmaschvanlu@gmail.com>
2023-12-31 18:17:05 +0100

    added framework call as in the clientside, an  rewrote the policeKeyPressed function (commented)

--- server.lua
+++ server.lua
@@ -1,20 +1,28 @@
-ESX = nil
-
-TriggerEvent('esx:getSharedObject', function(obj)
-    ESX = obj
-end)
-
+-- gets framework
+if (GetResourceState("es_extended") == "started") then
+  if (exports["es_extended"] and exports["es_extended"].getSharedObject) then
+    ESX = exports["es_extended"]:getSharedObject()
+  else
+    TriggerEvent("esx:getSharedObject", function(obj) ESX = obj end)
+  end
+end
+-- this happens wen a player presses a key,
 RegisterServerEvent('policeKeyPressed')
 AddEventHandler('policeKeyPressed', function()
-    local xPlayers = ESX.GetPlayers()
+    -- local xPlayers = ESX.GetPlayers() -- gets all players
 
-    for _, player in ipairs(xPlayers) do
-        local xPlayer = ESX.GetPlayerFromId(player)
+    -- for _, player in ipairs(xPlayers) do
+    --     local xPlayer = ESX.GetPlayerFromId(player)
 
-        if xPlayer.job.name == "police" then
-            TriggerClientEvent('sendNotificationToPolice', player, 'Ein Spieler hat den Panic Button gedrückt!')
-        end
-    end
+    --     if xPlayer.job.name == "police" then
+    --         TriggerClientEvent('sendNotificationToPolice', player, 'Ein Spieler hat den Panic Button gedrückt!')
+    --     end
+    -- end
+
+    local xPlayers = ESX.GetExtendedPlayers("job", "police") -- Returns all xPlayers with the job police, but doesnt work on esx version before 1.2
+  for _, xPlayer in pairs(xPlayers) do
+    TriggerClientEvent('sendNotificationToPolice', xPlayer.source, 'Ein Spieler hat den Panic Button gedrückt!')
+  end
 end)
 
 RegisterServerEvent('sendNotificationToPolice')
