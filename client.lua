022960dd2ac18e48ebb1ff81486cd3f7cd5cff35
Ludaro <waschmaschvanlu@gmail.com>
2023-12-31 18:12:29 +0100

    moved the thread and the framework to the bottom because it makes more sense,
    events->functions->code
    
    and added comments--

--- client.lua
+++ client.lua
@@ -1,3 +1,38 @@
+-- what happens when the police presses the key,
+-- this could also be a function, which is better but hey, as long as it works. it works.
+RegisterNetEvent('policeKeyPressedClient')
+AddEventHandler('policeKeyPressedClient', function()
+    local playerPed = PlayerPedId()
+    TriggerServerEvent('sendNotificationToPolice', 'Ein Spieler hat den Panic Button gedrückt!')
+    showInfobar('Drücke ~b~[E]~s~, um einen Wegpunkt zu setzen')
+    SetNewWaypoint(GetEntityCoords(GetPlayerPed(-1)))
+    local blip = AddBlipForCoord(GetEntityCoords(GetPlayerPed(-1)))
+    SetBlipSprite(blip, 161)
+    SetBlipColour(blip, 3)
+    SetBlipScale(blip, 1.0)
+    Wait(15000)
+    RemoveBlip(blip)
+end)
+-- event to give notification to police
+RegisterNetEvent('showPoliceNotification')
+AddEventHandler('showPoliceNotification', function(message)
+    ShowNotification(message)
+end)
+--function to show the notification to police
+function ShowNotification(text)
+    SetNotificationTextEntry('STRING')
+    AddTextComponentString(text)
+    DrawNotification(false, true)
+end
+-- function to show the infobar
+function showInfobar(msg)
+    CurrentActionMsg = msg
+    SetTextComponentFormat('STRING')
+    AddTextComponentString(CurrentActionMsg)
+    DisplayHelpTextFromStringLabel(0, 0, 1, -1)
+end
+
+-- gets framework
 if (GetResourceState("es_extended") == "started") then
   if (exports["es_extended"] and exports["es_extended"].getSharedObject) then
     ESX = exports["es_extended"]:getSharedObject()
@@ -5,7 +40,7 @@ if (GetResourceState("es_extended") == "started") then
     TriggerEvent("esx:getSharedObject", function(obj) ESX = obj end)
   end
 end
-
+--thread for pressing the button
 Citizen.CreateThread(function()
     while true do
         Citizen.Wait(0)
@@ -22,35 +57,3 @@ Citizen.CreateThread(function()
         end
     end
 end)
-
-RegisterNetEvent('policeKeyPressedClient')
-AddEventHandler('policeKeyPressedClient', function()
-    local playerPed = PlayerPedId()
-    TriggerServerEvent('sendNotificationToPolice', 'Ein Spieler hat den Panic Button gedrückt!')
-    showInfobar('Drücke ~b~[E]~s~, um einen Wegpunkt zu setzen')
-    SetNewWaypoint(GetEntityCoords(GetPlayerPed(-1)))
-    local blip = AddBlipForCoord(GetEntityCoords(GetPlayerPed(-1)))
-    SetBlipSprite(blip, 161)
-    SetBlipColour(blip, 3)
-    SetBlipScale(blip, 1.0)
-    Wait(15000)
-    RemoveBlip(blip)
-end)
-
-RegisterNetEvent('showPoliceNotification')
-AddEventHandler('showPoliceNotification', function(message)
-    ShowNotification(message)
-end)
-
-function ShowNotification(text)
-    SetNotificationTextEntry('STRING')
-    AddTextComponentString(text)
-    DrawNotification(false, true)
-end
-
-function showInfobar(msg)
-    CurrentActionMsg = msg
-    SetTextComponentFormat('STRING')
-    AddTextComponentString(CurrentActionMsg)
-    DisplayHelpTextFromStringLabel(0, 0, 1, -1)
-end
